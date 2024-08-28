f8b6d181764a66c1d706da1451057399e2b0c8bc
Eleiny9 <eleiny9@gmail.com>
2024-08-28 17:16:24 +0000

    se obtinene los datos

--- src/app.py
+++ src/app.py
@@ -8,18 +8,19 @@ import requests
 url = "https://ycharts.com/companies/TSLA/revenues"
 
 # Petición para descargar el fichero de Internet
-fichero = requests.get(url, time.sleep(10)).text
+fichero = requests.get(url, time.sleep(5)).text
 
 if "403 ERROR" in fichero:
     headers = {'User-Agent':"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
     request = requests.get(url,headers=headers)
-    time.sleep(10)
+    time.sleep(5)
     fichero = request.text
 
 
+soup = BeautifulSoup(fichero, "html.parser")
 
-td = fichero.find_all("td", class_="text-right")
+table = soup.find_all("table", class_="table")
 
-print(td)
+print(table)
 
 
