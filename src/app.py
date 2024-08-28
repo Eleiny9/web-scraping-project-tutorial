788c50b06b72bed8e0b4e7cfedb403ed3868204c
Eleiny9 <eleiny9@gmail.com>
2024-08-28 15:27:05 +0000

    cambios

--- src/app.py
+++ src/app.py
@@ -1,8 +1,22 @@
-import os
+import time
 from bs4 import BeautifulSoup
 import requests
-import time
-import sqlite3
-import matplotlib.pyplot as plt
-import seaborn as sns
+
+
+
+# Seleccionar el recurso a descargar
+url = "https://www.w3schools.com/python/ref_string_split.asp#:~:text=The%20split()%20method%20splits,default%20separator%20is%20any%20whitespace."
+
+# Petición para descargar el fichero de Internet
+fichero = requests.get(url, time.sleep(10))
+print(fichero)
+
+if fichero:
+    soup = BeautifulSoup( fichero.text , 'html')
+    print(soup)
+
+"""td = soup.find_all("td", class_="text-right")
+
+print(td)"""
+
 
